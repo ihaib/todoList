b5de2a4a4c98e3aa3da2cc834bd2a45568a61d57
ihaib <ihaib.ramdan@hotmail.fr>
2019-11-08 14:54:30 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoList';
+
+  constructor() {
+    const  firebaseConfig = {
+      apiKey: "AIzaSyA0UAI_trmgaRaiToLueneGXGFsnNcwY5g",
+      authDomain: "todolist-bc3ea.firebaseapp.com",
+      databaseURL: "https://todolist-bc3ea.firebaseio.com",
+      projectId: "todolist-bc3ea",
+      storageBucket: "todolist-bc3ea.appspot.com",
+      messagingSenderId: "399642025277",
+      appId: "1:399642025277:web:b1035d2737098735f2f954"
+    }
+    firebase.initializeApp(firebaseConfig);
+  }
+
 }
